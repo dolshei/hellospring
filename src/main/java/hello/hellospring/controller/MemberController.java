57a33177d6965d38e769acbc96d8e68f83168da5
LEE.CHANG.HUN <dolshei@gmail.com>
2022-07-03 23:21:29 +0900

    JDBC 방식과 JPA 방식에 대한 코드 추가

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,12 +3,14 @@ package hello.hellospring.controller;
 import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@Controller
 public class MemberController {
 
     private final MemberService memberService;
@@ -18,11 +20,12 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping(value = "/member/new")
+    @GetMapping(value = "/members/new")
     public String createForm() {
         return "members/createMemberForm";
     }
 
+//    회원 등록
     @PostMapping(value = "/members/new")
     public String create(MemberForm form) {
         Member member = new Member();
@@ -33,6 +36,7 @@ public class MemberController {
         return "redirect:/";
     }
 
+//    조회 기능
     @GetMapping(value = "/members")
     public String list(Model model) {
         List<Member> members = memberService.findMembers();
